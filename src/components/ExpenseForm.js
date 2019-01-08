493ff3a284a2871637a3ba2076902241d834e24b
Unknown <AlixAkhribi@yahoo.com>
2019-01-08 15:30:01 -0800

    Refactor ExpenseForm to be more flexible
    Used constructor for state. If expense already exists, use those values, if not (adding new expense) use empty values.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,14 +9,19 @@ console.log(now);
 
 
 export default class ExepenseForm extends React.Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        };
+    }
+
 
     onDescriptionChange = (e) => {
         const description = e.target.value;
