fbb8507d2c783c95b8f35639985021810b9ae0fd
Unknown <AlixAkhribi@yahoo.com>
2019-01-14 19:24:09 -0800

    Create default filter value, sortBy amount, sortBy date test cases for reducers/filters

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,23 @@
+import moment from 'moment';
+import filterReducer from '../../reducers/filters';
+
+test('should setup default filter values', () => {
+    const state = filterReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual({
+        text: '',
+        sortBy: 'date',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month')
+    });
+});
+
+test('should set sortBy to amount', () => {
+    const state = filterReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+    expect(state.sortBy).toEqual('amount')
+}
+
+)
+test('should set sortBy to date', () => {
+    const state = filterReducer(undefined, { type: 'SORT_BY_DATE' });
+    expect(state.sortBy).toEqual('date')
+})
