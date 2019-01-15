fbd285ca0050b65b632734706f77015af3b0ca7a
Unknown <AlixAkhribi@yahoo.com>
2019-01-14 17:25:51 -0800

    Create text, startDate, endDate test cases for Selector/Expenses, fix endDateMatch bug in Selectors/Expenses

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -2,12 +2,12 @@ import moment from 'moment';
 
 // Get Visible Expenses
 
-const getVisibleExpenses = (expenses, { text, sortBy, endDate, startDate }) => {
+export default (expenses, { text, sortBy, endDate, startDate }) => {
     return expenses.filter((expense) => {
 
         const momentCreatedAt = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(momentCreatedAt, 'day') : true;
-        const endDateMatch = endDate ? startDate.isSameOrAfter(momentCreatedAt, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(momentCreatedAt, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
@@ -21,7 +21,3 @@ const getVisibleExpenses = (expenses, { text, sortBy, endDate, startDate }) => {
     })
 }
 
-
-
-
-export default getVisibleExpenses
---
+++ src/tests/selectors/expenses.test.js
@@ -0,0 +1,59 @@
+import moment from 'moment'
+import selectExpenses from '../../selectors/expenses'
+
+
+// Expenses Dummy data
+
+const expenses = [{
+    id: 1,
+    description: 'Gum',
+    note: '',
+    amount: 140,
+    createdAt: 0
+}, {
+    id: 2,
+    description: 'Rent',
+    note: '',
+    amount: 190500,
+    createdAt: moment(0).subtract(4, 'days').valueOf()
+}, {
+    id: 3,
+    description: 'Credit Card',
+    note: '',
+    amount: 25000,
+    createdAt: moment(0).add(5, 'days').valueOf()
+}];
+
+test('Should filter by text Value', () => {
+    const filters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined,
+    };
+    const results = selectExpenses(expenses, filters);
+    // The credit card expense sould return first due to sortBy:date being the default 
+    expect(results).toEqual([expenses[2], expenses[1]])
+})
+
+test('Should filter by start date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined,
+    };
+    const results = selectExpenses(expenses, filters);
+    expect(results).toEqual([expenses[2], expenses[0]])
+})
+
+test('Should filter by end date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: moment(0).add(2, 'days')
+    };
+    const results = selectExpenses(expenses, filters);
+    expect(results).toEqual([expenses[0], expenses[1]])
+})
