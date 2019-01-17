357fac2faa893a278ad9cf8a25aba4afca031082
Unknown <AlixAkhribi@yahoo.com>
2019-01-16 19:34:50 -0800

    Create sortByDate, setStartDate, setEndDate test cases for reducers/filters

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import filterReducer from '../../reducers/filters';
+import { stat } from 'fs';
 
 test('should setup default filter values', () => {
     const state = filterReducer(undefined, { type: '@@INIT' });
@@ -14,10 +15,35 @@ test('should setup default filter values', () => {
 test('should set sortBy to amount', () => {
     const state = filterReducer(undefined, { type: 'SORT_BY_AMOUNT' });
     expect(state.sortBy).toEqual('amount')
-}
+});
 
-)
 test('should set sortBy to date', () => {
-    const state = filterReducer(undefined, { type: 'SORT_BY_DATE' });
+    const currentState = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    }
+    const state = filterReducer(currentState, { type: 'SORT_BY_DATE' });
     expect(state.sortBy).toEqual('date')
-})
+});
+
+test('should set start date filter ', () => {
+    const startDate = moment();
+    const action = {
+        type: 'SET_START_DATE',
+        startDate
+    }
+    const state = filterReducer(undefined, action);
+    expect(state.startDate).toEqual(startDate)
+});
+
+test('should set end date filter ', () => {
+    const endDate = moment();
+    const action = {
+        type: 'SET_END_DATE',
+        endDate
+    }
+    const state = filterReducer(undefined, action);
+    expect(state.endDate).toEqual(endDate)
+});
