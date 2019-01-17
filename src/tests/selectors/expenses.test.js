e8aa7db05dd41db3c4baf8e487d71792a9deaa10
Unknown <AlixAkhribi@yahoo.com>
2019-01-16 19:32:54 -0800

    Move expenses test data to fixtures test folder

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,28 +1,7 @@
 import moment from 'moment'
 import selectExpenses from '../../selectors/expenses'
+import expenses from '../fixtures/expenses'
 
-
-// Expenses Dummy data
-
-const expenses = [{
-    id: 1,
-    description: 'Gum',
-    note: '',
-    amount: 140,
-    createdAt: 0
-}, {
-    id: 2,
-    description: 'Rent',
-    note: '',
-    amount: 190500,
-    createdAt: moment(0).subtract(4, 'days').valueOf()
-}, {
-    id: 3,
-    description: 'Credit Card',
-    note: '',
-    amount: 25000,
-    createdAt: moment(0).add(5, 'days').valueOf()
-}];
 
 test('Should filter by text Value', () => {
     const filters = {
